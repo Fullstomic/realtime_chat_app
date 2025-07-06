dce2bc1b2169d31222979f1d5a259125ec446aea
Fullstomic-Developer <develop.masaharu@gmail.com>
2025-07-06 08:18:53 +0000

    サーバー側の処理完成

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   console.log("connection");
-  socket.on("chat message", (msg) => {
-    console.log("message" + msg);
+  socket.on("login", (username) => {
+    socket.username = username;
+    socket.emit("login", socket.username);
+  });
+  socket.on("chat message", (msg, username) => {
+    socket.emit("reception message", username, msg);
   });
   socket.on("disconnect", () => {
     console.log("user disconnected");
